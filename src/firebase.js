9aad17dc76065346a50c3a3d716d7a564e9f4a90
ADARSH KUMAR <61309352+ADDY-AK@users.noreply.github.com>
2021-04-29 19:56:39 +0530

    amazon nav bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBaM4zIAXbbikGOK0UcmCZqkOf-_ioMqgw",
+    authDomain: "clone-cf604.firebaseapp.com",
+    projectId: "clone-cf604",
+    storageBucket: "clone-cf604.appspot.com",
+    messagingSenderId: "976519084366",
+    appId: "1:976519084366:web:c12debfc898fc00386c61f",
+    measurementId: "G-N3Y5G6CDNM"
+  };
